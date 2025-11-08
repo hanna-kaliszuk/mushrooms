88351455c9020a798c9be9a29d7b6bff9c25ff90
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2025-11-08 13:56:46 +0100

    created needed arrays to store mushroom positions and DP values

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <vector>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+// constants
+const long long MOD = 1000000000;
+const int MAX_N = 1000;
+const int MAX_M = 1000;
+const int MAX_K = 10;
+
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+   int n, m, k , g;
+   cin >> n >> m >> k >> g;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+   // create a 2D bool array to mark mashrooms' positions
+   vector<vector<int>> hasMushroom(n, vector<int>(m, 0));
 
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+   for (int i = 0; i < g; i++) {
+      int a, b;
+      cin >> a >> b;
+      hasMushroom[a - 1][b - 1] = 1;
+   }
+
+   // create a 4D DP array iniciated with 0
+   // DP[i][j][col][dir] = number of ways to reach cell (i, j) coming from direction 'dir' with 'col' mushrooms collected
+   // dir: 0 = from left
+   //      1 = from above
+   //      2 = from below
+   // if >= k mushrooms collected, we consider it as k mushrooms collected
+
+   vector<vector<vector<vector<long long>>>> DP(
+      n,vector<vector<vector<long long>>>(m,vector<vector<long long>>(k,vector<long long>(3, 0LL))));
+
+
+
+   return 0;
 }
